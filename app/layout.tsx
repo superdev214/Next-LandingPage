e6a5877d0dcaad201cf1e0621e16334c86ad2f36
royalDev <royaldev.web.development@gmail.com>
2023-06-30 07:32:33 -0700

    add: add font files

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,7 +100,7 @@ function RootLayout({
               <Link
                 href="/"
                 className={
-                  "text-[17px] px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
+                  "text-[17px] px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative font-display" +
                   (activeLink === "testimoni"
                     ? " text-orange-500 animation-active "
                     : " text-black-500 hover:text-orange-500 ")
